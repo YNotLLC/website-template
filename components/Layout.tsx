7d9c6ec42320f935198bc5c88e23bb2cbbcf5499
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2023-04-04 01:10:13 +0000

    add header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,10 +4,10 @@ import { Header } from './Header'
 
 export const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <>
+    <div className="flex flex-col">
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      red: '#FF0000',
+      orange: '#FFD600',
+    },
     extend: {},
   },
   plugins: [],
